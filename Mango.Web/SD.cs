379d6ab3621334c974a987a9e23cc7b0f601fbd3
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2022-04-11 15:36:00 +0530

    service setup in mango

---
+++ Mango.Web/SD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web
+{
+    public class SD
+    {
+        public static string ProductAPIBase { get; set; }
+        public enum ApuType
+        {
+            GET,
+            PUT,
+            POST,
+            Delete
+        }
+    }
+}
---
+++ Mango.Web/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services
+{
+    public interface IProductService
+    {
+        Task<T> GetAllPrductsAync<T>();
+        Task<T> CreateProductAllSink<T>(ProductDto productDto);
+        Task<T> GetPrductById<T>(int id);
+        Task<T> UpdateProductById<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T> (int id);
+    }
+}
